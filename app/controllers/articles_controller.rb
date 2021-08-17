3092f33220ee9b2ba553193aa14e5ef80942ff9f
Andy <andreapauline.deguzman@gmail.com>
2021-08-17 17:21:29 +0800

    monday lecture routes, controllers,views

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new
+
+    @article.name = params[:name]
+    @article.body = params[:body]
+    if @article.save
+      redirect_to articles_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+  
+  end
+
+  def delete
+  
+  end
+end
