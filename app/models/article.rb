7b3af902197d29a440c34bfa5686cf920458d29d
Andy <andreapauline.deguzman@gmail.com>
2021-08-23 20:51:14 +0800

    adding files

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,5 +3,5 @@ class Article < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :body, presence: true, length: { minimum: 5 }
 
-  has_may :comments
+  has_many :comments
 end
